927fc4682b9fc1075c582b031c494f1fd23f4852
Tushar vishwakarma <realgamevishwakarma123@gmail.com>
2024-01-07 11:07:27 +0530

    first commit

--- gsap.js
+++ gsap.js
@@ -1,8 +1,8 @@
 let a = gsap.timeline()
 
 a.from("#logo",{
-    y:-30,
-    duration:2,
+    y:-20,
+    duration:1,
     delay:1,
     opacity:0,
     stagger:1,
@@ -11,13 +11,18 @@ a.from("#im",{
     x:30,
     duration:1,
     opacity:0,
-    scale:1.5,
+    // scale:1.5,
 })
 a.from("#im2",{
     x:-30,
     duration:1,
     opacity:0,
-    scale:1.5,
+    // scale:1.5,
+    
+})
+a.from("#text",{
+    duration:1,
+    opacity:0,
     
 })
 
@@ -55,7 +60,17 @@ gsap.from("#box",{
     scrub:5
 }
 })
-
-let menu = ()=>{
-    
+gsap.from("#pg3",{
+  opacity:0,
+x:100,
+  duration:3,
+  scrollTrigger:{
+    trigger:" #pg3",
+    scroller:"body",
+    markers:true,
+    start:"top 60%",
+    end:"top 100%",
+    scrub:5
 }
+})
+
